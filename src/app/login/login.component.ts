3a7d9a31182ef30d6383bc74925554bdccd2ea43
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-09-13 17:04:29 +1000

    rm server, mk new server w/ express-generator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,35 @@
+// login.component.ts
+
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        if (response && response.user) {
+          // Authentication successful, navigate to the groups page
+          this.router.navigate(['/groups']);
+        } else {
+          // Authentication failed, handle the error or display an error message
+          console.error('Authentication failed');
+        }
+      },
+      (error) => {
+        // Handle HTTP error or display an error message
+        console.error('An error occurred:', error);
+      }
+    );
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserModel } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Method for user login
+  login(username: string, password: string): Observable<{ message: string; user: UserModel } | null> {
+    return this.http.post<{ message: string; user: UserModel } | null>(`${this.apiUrl}/login`, {
+      username,
+      password,
+    });
+  }
 }
