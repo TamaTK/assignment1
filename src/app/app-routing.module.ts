2a2327d3fa8a63e60f2583ca8977111f7d55d4f6
Tamahou Scott <73814295+TamaTK@users.noreply.github.com>
2023-09-13 15:31:19 +1000

    Generate routes and update route component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+// imported components
+import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { GroupManagementComponent } from './group-management/group-management.component';
+import { ChannelManagementComponent } from './channel-management/channel-management.component';
+
+// imported other
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // generated component routes
+  { path: 'login', component: LoginComponent },
+  { path: 'groups', component: GroupsComponent },
+  { path: 'channels/:groupId', component: ChannelsComponent },
+  { path: 'user-management', component: UserManagementComponent },
+  { path: 'group-management', component: GroupManagementComponent },
+  { path: 'channel-management', component: ChannelManagementComponent },
+  // home
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
